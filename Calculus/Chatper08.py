914d3443237b7445e8981b9b4cd55a2924605633
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-05-09 11:22:44 +0900

    Add source code from textbook

--- Calculus/Chatper08.py
+++ Calculus/Chatper08.py
@@ -6,9 +6,11 @@ def average_flow_rate(v, t1: float, t2: float):
     """
     Calculate average flow rate, differential for delta
     Args:
-        v: volume
+        v: volume function
         t1: time stamp 1
         t2: time stamp 2
     """
     return (v(t2) - v(t1))/(t2 - t1)
+
+
 
